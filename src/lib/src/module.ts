9c97b04b11b9e795656e16dcda2e875c535670c0
Cyrille Tuzi <web@cyrilletuzi.com>
2017-11-29 16:27:14 +0100

    feat(lib): extensiblity

--- src/lib/src/module.ts
+++ src/lib/src/module.ts
@@ -8,39 +8,36 @@ import { IndexedDBDatabase } from './service/databases/indexeddb-database';
 import { LocalStorageDatabase } from './service/databases/localstorage-database';
 import { MockLocalDatabase } from './service/databases/mock-local-database';
 
-export function asyncLocalStorageFactory(platformId: Object, jsonValidator: JSONValidator) {
-
-  let database: AsyncLocalDatabase;
+export function databaseFactory(platformId: Object) {
 
   if (isPlatformBrowser(platformId) && ('indexedDB' in window)) {
 
     /* Try with IndexedDB in modern browsers */
-    database = new IndexedDBDatabase();
+    return new IndexedDBDatabase();
 
   } else if (isPlatformBrowser(platformId) && ('localStorage' in window)) {
 
     /* Try with localStorage in old browsers (IE9) */
-    database = new LocalStorageDatabase();
+    return new LocalStorageDatabase();
 
   } else {
 
     /* Fake database for server-side rendering (Universal) */
-    database = new MockLocalDatabase();
+    return new MockLocalDatabase();
 
   }
 
-  return new AsyncLocalStorage(database, jsonValidator);
-
-};
+}
 
 @NgModule({
   providers: [
     JSONValidator,
     {
-      provide: AsyncLocalStorage,
-      useFactory: asyncLocalStorageFactory,
-      deps: [PLATFORM_ID, JSONValidator]
+      provide: AsyncLocalDatabase,
+      useFactory: databaseFactory,
+      deps: [PLATFORM_ID]
     },
+    AsyncLocalStorage,
   ]
 })
 export class AsyncLocalStorageModule {}
