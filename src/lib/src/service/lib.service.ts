d1185e4ecbb4f3c65e5a2a098a5c01da516b9d56
Cyrille Tuzi <web@cyrilletuzi.com>
2018-04-07 15:10:39 +0200

    fix: correct type inference for getItem

--- src/lib/src/service/lib.service.ts
+++ src/lib/src/service/lib.service.ts
@@ -26,7 +26,7 @@ export class LocalStorage {
    * @param key The item's key
    * @returns The item's value if the key exists, null otherwise, wrapped in an RxJS Observable
    */
-  getItem<T = any>(key: string, options: LSGetItemOptions = this.getItemOptionsDefault) {
+  getItem<T = any>(key: string, options: LSGetItemOptions = this.getItemOptionsDefault): Observable<T | null> {
 
     return this.database.getItem<T>(key).pipe(
 
@@ -61,7 +61,7 @@ export class LocalStorage {
    * @param data The item's value, must NOT be null or undefined
    * @returns An RxJS Observable to wait the end of the operation
    */
-   setItem(key: string, data: any) {
+   setItem(key: string, data: any): Observable<boolean> {
 
     return this.database.setItem(key, data);
 
@@ -72,7 +72,7 @@ export class LocalStorage {
    * @param key The item's key
    * @returns An RxJS Observable to wait the end of the operation
    */
-   removeItem(key: string) {
+   removeItem(key: string): Observable<boolean> {
 
     return this.database.removeItem(key);
 
@@ -82,7 +82,7 @@ export class LocalStorage {
    * Deletes all items from local storage
    * @returns An RxJS Observable to wait the end of the operation
    */
-   clear() {
+   clear(): Observable<boolean> {
 
     return this.database.clear();
 
