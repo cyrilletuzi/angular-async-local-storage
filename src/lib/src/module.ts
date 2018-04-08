6e8570d4c62d377b5d9a0e2a25f6531ad9d0fff2
Cyrille Tuzi <web@cyrilletuzi.com>
2018-04-08 12:15:48 +0200

    docs: deprecated module

--- src/lib/src/module.ts
+++ src/lib/src/module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
 
+/** @deprecated Since version 6, you don't need to import this module anymore. Services are directly injectable. */
 @NgModule()
 export class LocalStorageModule {}
