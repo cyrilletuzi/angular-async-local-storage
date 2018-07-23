24c750855b7e96a402f9174574c32ef9ce9da04e
Cyrille Tuzi <web@cyrilletuzi.com>
2018-07-23 10:52:00 +0200

    tests: reintegrate karma config for all browsers

--- projects/ngx-pwa/local-storage/karma.conf.js
+++ projects/ngx-pwa/local-storage/karma.conf.js
@@ -8,6 +8,10 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
+      require('karma-safari-launcher'),
+      require('karma-edge-launcher'),
+      require('karma-ie-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -25,7 +29,41 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
+    customLaunchers: {
+      Chrome_private: {
+        base: 'Chrome',
+        flags: ['--incognito']
+      },
+      Firefox_private: {
+        base: 'Firefox',
+        flags: ['-private']
+      },
+      Edge_private: {
+        base: 'Edge',
+        flags: ['-private']
+      },
+      IE_private: {
+        base: 'IE',
+        flags: ['-private']
+      }
+    },
+    /* Quick tests */
     browsers: ['Chrome'],
+    /* Tests on Mac
+     * IMPORTANT FOR SAFARI: by default, Safari reopens previous tabs, meaning there can be multiple instances of the same tests running at the same time,
+     * which leads some tests to crash because of asynchronicty. To avoid this, manually close all Karma tabs in Safari, or run in the terminal:
+     * defaults write com.apple.Safari ApplePersistenceIgnoreState YES
+     */
+    // browsers: ['Chrome', 'Firefox', 'Safari', 'Chrome_private'],
+    /* Tests on Mac in private modes
+     * IMPORTANT : Safari does not have a command line option for private mode (if you know one, please tell me),
+     * so you have to manually configure it to open a new private tab on launch, before launching the tests */
+    // browsers: ['Chrome_private', 'Firefox_private', 'Safari'],
+    /* Tests on Windows */
+    // browsers: ['Chrome', 'Firefox', 'Edge', 'IE'],
+    /* Tests on Windows in private mode
+     * Not sure the private option for Edge is working... */
+    // browsers: ['Chrome_private', 'Firefox_private', 'Edge_private', 'IE_private'],
     singleRun: false
   });
 };
