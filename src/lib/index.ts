b702c324044441573aeab5931f86298faee69efd
Cyrille Tuzi <web@cyrilletuzi.com>
2018-07-23 10:03:48 +0200

    feat: CLI project

--- src/lib/index.ts
+++
@@ -1,8 +0,0 @@
-export { JSONSchema } from './src/service/validation/json-schema';
-export { LocalDatabase } from './src/service/databases/local-database';
-export { IndexedDBDatabase } from './src/service/databases/indexeddb-database';
-export { LocalStorageDatabase } from './src/service/databases/localstorage-database';
-export { MockLocalDatabase } from './src/service/databases/mock-local-database';
-export { JSONValidator } from './src/service/validation/json-validator';
-export { LSGetItemOptions, LocalStorage } from './src/service/lib.service';
-export { localStorageProviders, LocalStorageProvidersConfig, LOCAL_STORAGE_PREFIX } from './src/tokens';
