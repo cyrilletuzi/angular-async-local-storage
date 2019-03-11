08cb4368b87c6b56956ed9f10dae833ef8b3df89
Cyrille Tuzi <web@cyrilletuzi.com>
2019-03-11 21:53:33 +0100

    tests: e2e tests

--- projects/demo-e2e/src/app.e2e-spec.ts
+++ projects/demo-e2e/src/app.e2e-spec.ts
@@ -1,23 +1,75 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, $ } from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe('public api', () => {
 
-  beforeEach(() => {
-    page = new AppPage();
+  beforeAll(async () => {
+
+    await browser.get('/');
+
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to demo!');
+  it('getItem()', async () => {
+
+    const data = await $('#get-item').getText();
+
+    expect(data).toBe('hello world');
+
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    }));
+  it('schema error', async () => {
+
+    const data = await $('#schema-error').getText();
+
+    expect(data).toBe('schema error');
+
   });
+
+  it('removeItem()', async () => {
+
+    const data = await $('#remove-item').getText();
+
+    expect(data).toBe('');
+
+  });
+
+  it('clear()', async () => {
+
+    const data = await $('#clear').getText();
+
+    expect(data).toBe('');
+
+  });
+
+  it('size()', async () => {
+
+    const data = Number.parseInt(await $('#size').getText(), 10);
+
+    expect(data).not.toBeNaN();
+
+  });
+
+  it('length', async () => {
+
+    const data = Number.parseInt(await $('#length').getText(), 10);
+
+    expect(data).not.toBeNaN();
+
+  });
+
+  it('keys()', async () => {
+
+    const data = await $('#keys').getText();
+
+    expect(data).not.toBe('');
+
+  });
+
+  it('has', async () => {
+
+    const data = await $('#has').getAttribute('hidden');
+
+    expect(data).toBe('true');
+
+  });
+
 });
--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { LocalStorage, JSONSchema } from '@ngx-pwa/local-storage';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, catchError } from 'rxjs/operators';
 
 interface Data {
   title: string;
@@ -10,30 +10,76 @@ interface Data {
 @Component({
   selector: 'app-root',
   template: `
-    <h1>{{(data$ | async)?.title}}</h1>
+    <p id="get-item">{{(getItem$ | async)?.title}}</p>
+    <p id="schema-error">{{schemaError$ | async}}</p>
+    <p id="remove-item">{{removeItem$ | async}}</p>
+    <p id="clear">{{clear$ | async}}</p>
+    <p id="size">{{size$ | async}}</p>
+    <p id="length">{{length$ | async}}</p>
+    <p id="keys">{{keys$ | async | json}}</p>
+    <p id="has" hidden="!(has$ | async)"></p>
     <iframe src="http://localhost:8080"></iframe>
   `
 })
 export class AppComponent implements OnInit {
 
-  title = 'LocalStorage';
-  data$: Observable<Data | null> | null = null;
+  getItem$!: Observable<Data | null>;
+  schemaError$!: Observable<string | null>;
+  removeItem$!: Observable<string | null>;
+  clear$!: Observable<string | null>;
+  size$!: Observable<number>;
+  length$!: Observable<number>;
+  keys$!: Observable<string[]>;
+  has$!: Observable<boolean>;
 
   constructor(private localStorage: LocalStorage) {}
 
   ngOnInit() {
 
-    const key = 'test';
     const schema: JSONSchema = {
       type: 'object',
       properties: {
-        title: {
-          type: 'string'
-        }
-      }
+        title: { type: 'string' },
+      },
+      required: ['title'],
     };
 
-    this.data$ = this.localStorage.setItem(key, { title: this.title }).pipe(switchMap(() => this.localStorage.getItem(key, schema)));
+    this.getItem$ = this.localStorage.setItem('getItemTest', { title: 'hello world' }).pipe(
+      switchMap(() => this.localStorage.getItem<Data>('getItemTest', schema)),
+    );
+
+    this.schemaError$ = this.localStorage.setItem('schemaError', { wrong: 'test' }).pipe(
+      switchMap(() => this.localStorage.getItem('schemaError', schema)),
+      catchError(() => of('schema error')),
+    );
+
+    this.removeItem$ = this.localStorage.setItem('removeItem', 'test').pipe(
+      switchMap(() => this.localStorage.removeItem('removeItem')),
+      switchMap(() => this.localStorage.getItem<string>('removeItem', schema)),
+    );
+
+    this.clear$ = this.localStorage.setItem('clear', 'test').pipe(
+      switchMap(() => this.localStorage.clear()),
+      switchMap(() => this.localStorage.getItem<string>('clear', schema)),
+    );
+
+    this.size$ = this.localStorage.setItem('size1', 'test').pipe(
+      switchMap(() => this.localStorage.setItem('size2', 'test')),
+      switchMap(() => this.localStorage.size),
+    );
+
+    this.length$ = this.localStorage.setItem('size1', 'test').pipe(
+      switchMap(() => this.localStorage.setItem('size2', 'test')),
+      switchMap(() => this.localStorage.length),
+    );
+
+    this.keys$ = this.localStorage.setItem('keys', 'test').pipe(
+      switchMap(() => this.localStorage.keys()),
+    );
+
+    this.has$ = this.localStorage.setItem('has', 'test').pipe(
+      switchMap(() => this.localStorage.has('has')),
+    );
 
   }
 
