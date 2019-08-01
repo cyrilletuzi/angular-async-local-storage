647705289c07cff71ee8ee9442235b4d02b92b67
Cyrille Tuzi <web@cyrilletuzi.com>
2019-08-01 12:23:26 +0200

    feat: Angular 8.2 and TypeScript 3.5 and align configs

--- projects/ngx-pwa/local-storage/karma.conf.js
+++ projects/ngx-pwa/local-storage/karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../../coverage'),
-      reports: ['html', 'lcovonly'],
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
