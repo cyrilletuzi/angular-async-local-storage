a4f1735c34aca99301ee1fa3545a6a6c90e65053
Cyrille Tuzi <web@cyrilletuzi.com>
2019-08-19 17:01:00 +0200

    feat: make ng add works with sub applications

--- projects/ngx-pwa/local-storage/schematics/ng-add/index.ts
+++ projects/ngx-pwa/local-storage/schematics/ng-add/index.ts
@@ -3,19 +3,18 @@ import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 import { addImportToModule } from '@schematics/angular/utility/ast-utils';
 import { InsertChange } from '@schematics/angular/utility/change';
 import { addPackageJsonDependency, NodeDependencyType, getPackageJsonDependency } from '@schematics/angular/utility/dependencies';
+import { getAppModulePath } from '@schematics/angular/utility/ng-ast-utils';
+import { getWorkspace } from '@schematics/angular/utility/workspace';
+import { getWorkspace as getWorkspaceConfig } from '@schematics/angular/utility/config';
 import * as ts from 'typescript';
+
+import { Schema as StorageAddOptions } from './schema';
 
 const packageName = '@ngx-pwa/local-storage';
 const packageVersionLatest = '^8.0.0';
 
 function addDependency(angularMajorVersion: number): Rule {
   return (host: Tree) => {
-
-    /* Do not install twice */
-    const currentDependency = getPackageJsonDependency(host, packageName);
-    if (currentDependency !== null) {
-      throw new SchematicsException(`${packageName} is already installed, please use ng update instead`);
-    }
 
     /* Set lib version depending on Angular version */
     let packageVersion = packageVersionLatest;
@@ -37,28 +36,31 @@ function addDependency(angularMajorVersion: number): Rule {
   };
 }
 
-function addModule(angularMajorVersion: number): Rule {
+function addModule(mainPath: string, angularMajorVersion: number): Rule {
   return (host: Tree) => {
 
-    if (angularMajorVersion === 8) {
+    if (angularMajorVersion >= 8) {
 
-      const appModulePath = 'src/app/app.module.ts';
       const storageModuleName = `StorageModule.forRoot({ IDBNoWrap: true })`;
+      const appModulePath = getAppModulePath(host, mainPath);
 
       if (!host.exists(appModulePath)) {
         throw new SchematicsException(`Can't find AppModule`);
       }
 
       const appModuleFile = (host.read(appModulePath) as Buffer).toString('utf-8');
+
+      if (appModuleFile.includes('StorageModule')) {
+        throw new SchematicsException(`This project already uses StorageModule, please use 'ng update ${packageName}' instead`);
+      }
+
       const appModuleSource = ts.createSourceFile(appModulePath, appModuleFile, ts.ScriptTarget.Latest, true);
 
-      const appModuleChanges = addImportToModule(appModuleSource, appModulePath, storageModuleName, packageName);
+      const appModuleChanges = addImportToModule(appModuleSource, appModulePath, storageModuleName, packageName) as InsertChange[];
 
       const recorder = host.beginUpdate(appModulePath);
-      appModuleChanges.forEach(change => {
-        if (change instanceof InsertChange) {
-          recorder.insertLeft(change.pos, change.toAdd);
-        }
+      appModuleChanges.forEach((change) => {
+        recorder.insertLeft(change.pos, change.toAdd);
       });
       host.commitUpdate(recorder);
 
@@ -69,8 +71,29 @@ function addModule(angularMajorVersion: number): Rule {
   };
 }
 
-export default function ngAdd(): Rule {
-  return (host: Tree, context: SchematicContext) => {
+export default function ngAdd(options: StorageAddOptions): Rule {
+  return async (host: Tree, context: SchematicContext) => {
+
+    /* Get project */
+    const workspace = await getWorkspace(host);
+    const workspaceConfig = getWorkspaceConfig(host);
+    const projectName = options.project || workspaceConfig.defaultProject || '';
+    const project = workspace.projects.get(projectName);
+    if (options.project && !project) {
+      throw new SchematicsException(`Invalid project name '${options.project}'`);
+    }
+    if (!project) {
+      throw new SchematicsException(`Can't find a default project, try 'ng add ${packageName} --project yourprojectname'`);
+    }
+    if (project.extensions.projectType !== 'application') {
+      throw new SchematicsException(`'ng add ${packageName}' can only be used for projects with 'projectType': 'application'`);
+    }
+
+    const buildTarget = project.targets.get('build');
+    if (!buildTarget || !buildTarget.options || !buildTarget.options.main) {
+      throw new SchematicsException(`Your angular.json project config is broken, can't find 'architect.build.options.main'`);
+    }
+    const mainPath = buildTarget.options.main as string;
 
     /* Check Angular version */
     const angularDependency = getPackageJsonDependency(host, '@angular/core');
@@ -84,7 +107,7 @@ export default function ngAdd(): Rule {
 
     return chain([
       addDependency(angularMajorVersion),
-      addModule(angularMajorVersion),
+      addModule(mainPath, angularMajorVersion),
     ]);
 
   };
