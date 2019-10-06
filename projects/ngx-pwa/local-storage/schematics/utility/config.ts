1561b272a95cf7fc555d5912fe6f536fd7cddb34
Cyrille Tuzi <web@cyrilletuzi.com>
2019-10-06 21:30:44 +0200

    feat: update schematics for Angular 9

--- projects/ngx-pwa/local-storage/schematics/utility/config.ts
+++ projects/ngx-pwa/local-storage/schematics/utility/config.ts
@@ -5,8 +5,9 @@ import { getWorkspace as getWorkspaceConfig } from '@schematics/angular/utility/
 
 export const packageName = '@ngx-pwa/local-storage';
 // TODO: Automate this
-export const packageVersionLatest = '^8.2.3';
-export const packageVersionLTS = '^6.2.5';
+export const packageVersionLatest = '^9.0.0-beta.2';
+export const packageVersionLTS8 = '^8.2.3';
+export const packageVersionLTS7 = '^6.2.5';
 
 export function getAngularMajorVersion(host: Tree): number {
 
