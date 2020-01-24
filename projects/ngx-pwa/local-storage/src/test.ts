4a9e9112c7e9a05fc6d3bc6261d229935d7c0971
Cyrille Tuzi <web@cyrilletuzi.com>
2020-01-24 10:12:11 +0100

    tests: core-js issue

--- projects/ngx-pwa/local-storage/src/test.ts
+++ projects/ngx-pwa/local-storage/src/test.ts
@@ -1,6 +1,6 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'core-js';
+import 'core-js/stable';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
