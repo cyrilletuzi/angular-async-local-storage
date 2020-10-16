5da56a5d4bafad9e3080b7f17690c1d7df1ff914
Cyrille Tuzi <web@cyrilletuzi.com>
2020-10-16 19:21:16 +0200

    fix: align to new karma coverage config

--- projects/ngx-pwa/local-storage/karma.conf.js
+++ projects/ngx-pwa/local-storage/karma.conf.js
@@ -14,16 +14,20 @@ module.exports = function (config) {
       require('karma-edge-launcher'),
       require('karma-ie-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/ngx-pwa/local-storage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+    coverageReporter: {
+      dir: require('path').join(__dirname, '../../coverage/ngx-pwa/local-storage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcov' },
+        { type: 'text-summary' },
+      ],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
