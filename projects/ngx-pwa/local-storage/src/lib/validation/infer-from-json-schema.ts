931a7873a8a5d463a04eb649eea17a06bb1efb77
Cyrille Tuzi <web@cyrilletuzi.com>
2020-10-23 14:30:12 +0200

    infer arrays

--- projects/ngx-pwa/local-storage/src/lib/validation/infer-from-json-schema.ts
+++ projects/ngx-pwa/local-storage/src/lib/validation/infer-from-json-schema.ts
@@ -1,5 +1,25 @@
 import { JSONSchema } from './json-schema';
 
+export type InferFromJsonSchemaTuple<Schemas extends readonly JSONSchema[]> =
+  Schemas extends readonly [JSONSchema, JSONSchema] ? [InferFromJsonSchema<Schemas[0]>, InferFromJsonSchema<Schemas[1]>] :
+  Schemas extends readonly [JSONSchema, JSONSchema, JSONSchema] ? [InferFromJsonSchema<Schemas[0]>, InferFromJsonSchema<Schemas[1]>, InferFromJsonSchema<Schemas[2]>] :
+  // tslint:disable-next-line: max-line-length
+  Schemas extends readonly [JSONSchema, JSONSchema, JSONSchema, JSONSchema] ? [InferFromJsonSchema<Schemas[0]>, InferFromJsonSchema<Schemas[1]>, InferFromJsonSchema<Schemas[2]>, InferFromJsonSchema<Schemas[3]>] :
+  // tslint:disable-next-line: max-line-length
+  Schemas extends readonly [JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema] ? [InferFromJsonSchema<Schemas[0]>, InferFromJsonSchema<Schemas[1]>, InferFromJsonSchema<Schemas[2]>, InferFromJsonSchema<Schemas[3]>, InferFromJsonSchema<Schemas[4]>] :
+  // tslint:disable-next-line: max-line-length
+  Schemas extends readonly [JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema] ? [InferFromJsonSchema<Schemas[0]>, InferFromJsonSchema<Schemas[1]>, InferFromJsonSchema<Schemas[2]>, InferFromJsonSchema<Schemas[3]>, InferFromJsonSchema<Schemas[4]>, InferFromJsonSchema<Schemas[5]>] :
+  // tslint:disable-next-line: max-line-length
+  Schemas extends readonly [JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema] ? [InferFromJsonSchema<Schemas[0]>, InferFromJsonSchema<Schemas[1]>, InferFromJsonSchema<Schemas[2]>, InferFromJsonSchema<Schemas[3]>, InferFromJsonSchema<Schemas[4]>, InferFromJsonSchema<Schemas[5]>, InferFromJsonSchema<Schemas[6]>] :
+  // tslint:disable-next-line: max-line-length
+  Schemas extends readonly [JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema] ? [InferFromJsonSchema<Schemas[0]>, InferFromJsonSchema<Schemas[1]>, InferFromJsonSchema<Schemas[2]>, InferFromJsonSchema<Schemas[3]>, InferFromJsonSchema<Schemas[4]>, InferFromJsonSchema<Schemas[5]>, InferFromJsonSchema<Schemas[6]>, InferFromJsonSchema<Schemas[7]>] :
+  // tslint:disable-next-line: max-line-length
+  Schemas extends readonly [JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema] ? [InferFromJsonSchema<Schemas[0]>, InferFromJsonSchema<Schemas[1]>, InferFromJsonSchema<Schemas[2]>, InferFromJsonSchema<Schemas[3]>, InferFromJsonSchema<Schemas[4]>, InferFromJsonSchema<Schemas[5]>, InferFromJsonSchema<Schemas[6]>, InferFromJsonSchema<Schemas[7]>, InferFromJsonSchema<Schemas[8]>] :
+  // tslint:disable-next-line: max-line-length
+  Schemas extends readonly [JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema, JSONSchema] ? [InferFromJsonSchema<Schemas[0]>, InferFromJsonSchema<Schemas[1]>, InferFromJsonSchema<Schemas[2]>, InferFromJsonSchema<Schemas[3]>, InferFromJsonSchema<Schemas[4]>, InferFromJsonSchema<Schemas[5]>, InferFromJsonSchema<Schemas[6]>, InferFromJsonSchema<Schemas[7]>, InferFromJsonSchema<Schemas[8]>, InferFromJsonSchema<Schemas[9]>] :
+  unknown[]
+;
+
 export type InferFromJsonSchema<Schema extends JSONSchema, DefaultType = unknown> =
   Schema extends { const: infer ConstType } ? ConstType :
   Schema extends { enum: ReadonlyArray<infer EnumType> } ? EnumType :
@@ -7,10 +27,9 @@ export type InferFromJsonSchema<Schema extends JSONSchema, DefaultType = unknown
   Schema extends { type: 'integer' } ? number :
   Schema extends { type: 'number' } ? number :
   Schema extends { type: 'boolean' } ? boolean :
-  // Schema extends { type: 'array', items: infer ItemsType } ?
-  //   (ItemsType extends JSONSchema ? InferFromJsonSchema<ItemsType>[] :
-  //     (ItemsType extends ReadonlyArray<infer ItemType> ? (ItemType extends JSONSchema ? InferFromJsonSchema<ItemType>[] : unknown) : unknown)
-  //   ) :
+  Schema extends { type: 'array', items: infer ItemsType } ?
+    ItemsType extends JSONSchema ? InferFromJsonSchema<ItemsType>[] :
+      ItemsType extends readonly JSONSchema[] ? InferFromJsonSchemaTuple<ItemsType> : unknown[] :
   Schema extends { type: 'object', properties: infer Properties } ?
-    { [Key in keyof Properties]: (Properties[Key] extends JSONSchema ? InferFromJsonSchema<Properties[Key]> : unknown) } :
+    { [Key in keyof Properties]: Properties[Key] extends JSONSchema ? InferFromJsonSchema<Properties[Key]> : unknown } :
     DefaultType;
