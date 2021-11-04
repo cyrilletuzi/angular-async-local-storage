50dfb13de77f326d27f8fdbc451cea4cc69d7efd
Cyrille Tuzi <web@cyrilletuzi.com>
2021-11-04 10:42:31 +0100

    fix

--- projects/ngx-pwa/local-storage/src/test.ts
+++ projects/ngx-pwa/local-storage/src/test.ts
@@ -1,6 +1,5 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import 'core-js/stable';
 import 'zone.js';
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
