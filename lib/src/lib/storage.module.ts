51267e5d4869f350615c700e6cc81852ffaafe82
Cyrille Tuzi <web@cyrilletuzi.com>
2024-11-16 10:51:45 +0100

    fix lint

--- lib/src/lib/storage.module.ts
+++ lib/src/lib/storage.module.ts
@@ -18,9 +18,13 @@ export class StorageModule {
     return {
       ngModule: StorageModule,
       providers: [
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
         config.LSPrefix !== undefined ? { provide: LS_PREFIX, useValue: config.LSPrefix } : [],
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
         config.IDBDBName !== undefined ? { provide: IDB_DB_NAME, useValue: config.IDBDBName } : [],
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
         config.IDBStoreName !== undefined ? { provide: IDB_STORE_NAME, useValue: config.IDBStoreName } : [],
+        // eslint-disable-next-line @typescript-eslint/no-deprecated
         config.IDBDBVersion !== undefined ? { provide: IDB_DB_VERSION, useValue: config.IDBDBVersion } : [],
         (config.IDBNoWrap === false) ? { provide: IDB_NO_WRAP, useValue: config.IDBNoWrap } : [],
       ],
