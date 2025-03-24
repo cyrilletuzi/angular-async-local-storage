2de2766522c661a991cb41d390a6707b7a6bee87
Cyrille Tuzi <555867+cyrilletuzi@users.noreply.github.com>
2025-03-24 19:22:29 +0100

    new prefer-contextual-for-variables lint rule

--- eslint.config.js
+++ eslint.config.js
@@ -159,7 +159,8 @@ module.exports = tseslint.config(
       "@angular-eslint/template/cyclomatic-complexity": "error",
       "@angular-eslint/template/no-duplicate-attributes": "error",
       "@angular-eslint/template/no-interpolation-in-attributes": "error",
-      "@angular-eslint/template/prefer-static-string-properties": 'error',
+      "@angular-eslint/template/prefer-static-string-properties": "error",
+      "@angular-eslint/template/prefer-contextual-for-variables": "error",
       "@angular-eslint/template/no-inline-styles": [
         "error",
         {
